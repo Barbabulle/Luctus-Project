f8360defcbc0fcb345357443223c6a275802a1a5
Barbabulle <nino.sciabbarrasi.auditeur@lecnam.net>
2021-05-19 14:20:05 +0200

    Minimap

---
+++ Assets/scripts/camera/Minimap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+}
