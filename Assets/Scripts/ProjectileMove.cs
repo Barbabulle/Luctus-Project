26c84a33e72bf5328f5e105ac6f41974d7da2b52
Barbabulle <nino.sciabbarrasi.auditeur@lecnam.net>
2021-06-06 12:55:46 +0200

    Scripts pas encore prêts

---
+++ Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    [SerializeField]
+    private float fireRate = 0.25f;
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    private Camera camera;
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed != 0)
+        {
+            transform.position += camera.transform.forward * (speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("pas de vitesse");
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnProjectiles.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    private GameObject effectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnVFX();
+        }
+    }
+
+   private void SpawnVFX()
+    {
+        GameObject vfx;
+        if(firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
+
